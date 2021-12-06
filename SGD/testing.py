32eb33a3687ccad25de530a246c4e9254b61e19d
Sonali <sonalikadham01@gmail.com>
2021-12-06 15:59:34 +0530

    SGD

---
+++ SGD/testing.py
@@ -0,0 +1,82 @@
+import numpy 
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+from pyspark import SparkContext
+from pyspark.sql.types import Row, StringType, StructField, StructType
+from pyspark.streaming import StreamingContext
+import json
+import pickle
+from pyspark.ml import Pipeline
+from pyspark.ml.feature import RegexTokenizer,StopWordsRemover, CountVectorizer,IDF,StringIndexer
+from pyspark.ml.feature import VectorAssembler
+from pyspark.sql.functions import length
+from pyspark.sql import SparkSession
+from pyspark.sql import Row
+from pyspark.ml.feature import CountVectorizer,StopWordsRemover,StringIndexer
+from pyspark.ml.classification import NaiveBayes
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, classification_report, confusion_matrix
+sc = SparkContext("local[2]", "spam").getOrCreate() #no of threads to run it, cluster name 
+ssc = StreamingContext(sc, 1)
+spark = SparkSession(sc)
+lines = ssc.socketTextStream("localhost", 6100)
+# Use defaults
+nb = NaiveBayes()
+
+def readStream(rdd):
+  if not rdd.isEmpty():
+    rddStream=rdd.collect()
+    array_of_vals=[i for rdd_val in rddStream for i in list(json.loads(rdd_val).values())]
+    schema=StructType([
+      StructField('subject',StringType(),False),
+      StructField('content',StringType(),False),
+      StructField('verdict',StringType(),False)
+    ])
+    df=spark.createDataFrame((Row(**d) for d in array_of_vals),schema)
+    df=df['content','verdict']
+    #df.show()
+    data = df.withColumn('length',length(df['content']))
+    # tokenizer = Tokenizer(inputCol="content", outputCol="token_content")
+    # stopremove = StopWordsRemover(inputCol='token_content',outputCol='stop_tokens')
+    # count_vec = CountVectorizer(inputCol='stop_tokens',outputCol='count_vec')
+    # idf = IDF(inputCol="count_vec", outputCol="tf_idf")
+    # ham_spam_to_num = StringIndexer(inputCol='verdict',outputCol='label')
+    # clean_up = VectorAssembler(inputCols=['tf_idf','length'],outputCol='features')
+    # data_prep_pipe = Pipeline(stages=[ham_spam_to_num,tokenizer,stopremove,count_vec,idf,clean_up])
+    # cleaner = data_prep_pipe.fit(data)
+    # clean_data = cleaner.transform(data)
+    stages=[]
+    regexTokenizer=RegexTokenizer(inputCol='content',outputCol='token',pattern='\\W+')
+    stages+=[regexTokenizer]
+    stopremove = StopWordsRemover(inputCol='token',outputCol='stop_tokens')
+    stages+=[stopremove]
+    cv=CountVectorizer(inputCol='stop_tokens',outputCol='token_features',minDF=2.0,vocabSize=2000)
+    stages+=[cv]
+    indexer=StringIndexer(inputCol='verdict',outputCol='numericlabel')
+    stages+=[indexer]
+    vectorassemble=VectorAssembler(inputCols=['token_features','length'],outputCol="features")
+    stages+=[vectorassemble]
+    pipeline=Pipeline(stages=stages)
+    clean_data=pipeline.fit(data).transform(data)
+    clean_data_np_X=numpy.array(clean_data.select('features').collect())
+    #print(clean_data_np_X)
+    clean_data_np_X=[i.flatten() for i in clean_data_np_X]
+    clean_data_np_X=numpy.array(clean_data_np_X)
+    #clean_data_np_X=numpy.pad(clean_data_np_X,2741-len(clean_data_np_X),'edge')
+    #print(type(clean_data_np_X))
+    filename='sgd_model'
+    load_model=pickle.load(open(filename,'rb'))
+    #test_results = load_nbmodel.predict(clean_data_np_X)
+    actual_test_op=numpy.array(clean_data.select('numericlabel').collect()).flatten()
+    #accuracy=load_nbmodel.score(clean_data_np_X,actual_test_op)
+    test_output=load_model.predict(clean_data_np_X)
+
+    #metrics = MulticlassMetrics(preds_and_labels.rdd.map(tuple))
+    #print(metrics.confusionMatrix().toArray())
+    print(accuracy_score(actual_test_op,test_output)*100)
+    print(precision_score(actual_test_op,test_output))
+    print("Recall:{}".format(recall_score(actual_test_op,test_output)))
+    print("F1 Score:{}".format(f1_score(actual_test_op,test_output,labels=np.unique(test_output))))
+
+lines.foreachRDD( lambda rdd: readStream(rdd) )
+ssc.start()
+ssc.awaitTermination()
---
+++ SGD/training.py
@@ -0,0 +1,93 @@
+import numpy as np 
+from sklearn.linear_model import SGDClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import make_pipeline
+import numpy
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+from pyspark import SparkContext
+from pyspark.sql.types import Row, StringType, StructField, StructType
+from pyspark.streaming import StreamingContext
+import json
+import pickle
+from pyspark.ml import Pipeline
+from pyspark.ml.feature import RegexTokenizer, Tokenizer,StopWordsRemover, CountVectorizer,IDF,StringIndexer
+from pyspark.ml.feature import VectorAssembler
+from pyspark.sql.functions import count, length
+from pyspark.sql import SparkSession
+from pyspark.sql import Row
+from pyspark.sql.functions import col, split
+from pyspark.ml.feature import CountVectorizer,StopWordsRemover,StringIndexer
+from pyspark.ml.classification import NaiveBayes
+from sklearn.naive_bayes import GaussianNB, MultinomialNB
+sc = SparkContext("local[2]", "spam").getOrCreate() #no of threads to run it, cluster name 
+ssc = StreamingContext(sc, 1)
+spark = SparkSession(sc)
+lines = ssc.socketTextStream("localhost", 6100)
+# Use defaults
+nb = NaiveBayes()
+count_batch_number=0
+def readStream(rdd):
+  
+  global count_batch_number
+  count_batch_number+=1
+  if not rdd.isEmpty():
+    rddStream=rdd.collect()
+    array_of_vals=[i for rdd_val in rddStream for i in list(json.loads(rdd_val).values())]
+    schema=StructType([
+      StructField('subject',StringType(),False),
+      StructField('content',StringType(),False),
+      StructField('verdict',StringType(),False)
+    ])
+    df=spark.createDataFrame((Row(**d) for d in array_of_vals),schema)
+    df=df['content','verdict']
+    #df.show()
+    #data = df.withColumn('length',length(df['content']))
+    #tokenizer = Tokenizer(inputCol="content", outputCol="token_content")
+    #stopremove = StopWordsRemover(inputCol='token_content',outputCol='stop_tokens')
+    #count_vec = CountVectorizer(inputCol='stop_tokens',outputCol='count_vec')
+    #idf = IDF(inputCol="count_vec", outputCol="tf_idf")
+    #ham_spam_to_num = StringIndexer(inputCol='verdict',outputCol='label')
+    #clean_up = VectorAssembler(inputCols=['tf_idf','length'],outputCol='features')
+    #data_prep_pipe = Pipeline(stages=[ham_spam_to_num,tokenizer,stopremove,count_vec,idf,clean_up])
+    #cleaner = data_prep_pipe.fit(data)
+    #clean_data = cleaner.transform(data)
+    data = df.withColumn('length',length(df['content']))
+    stages=[]
+    regexTokenizer=RegexTokenizer(inputCol='content',outputCol='token',pattern='\\W+')
+    stages+=[regexTokenizer]
+    stopremove = StopWordsRemover(inputCol='token',outputCol='stop_tokens')
+    stages+=[stopremove]
+    cv=CountVectorizer(inputCol='stop_tokens',outputCol='token_features',minDF=2.0,vocabSize=2000)
+    stages+=[cv]
+    indexer=StringIndexer(inputCol='verdict',outputCol='numericlabel')
+    stages+=[indexer]
+    vectorassemble=VectorAssembler(inputCols=['token_features','length'],outputCol="features")
+    stages+=[vectorassemble]
+    pipeline=Pipeline(stages=stages)
+    clean_data=pipeline.fit(data).transform(data)
+    #clean_data.show()
+    clean_data_np_X=numpy.array(clean_data.select('features').collect())
+    #print(clean_data_np_X)
+    clean_data_np_y=numpy.array(clean_data.select('numericlabel').collect()).flatten()
+    clean_data_np_X=[i.flatten() for i in clean_data_np_X]
+    clean_data_np_X=numpy.array(clean_data_np_X)
+    #nb=MultinomialNB()
+    #print(clean_data_np_X)
+    #print(clean_data_np_y)
+    #clf = make_pipeline(StandardScaler(),
+                     #SGDClassifier(max_iter=1000, tol=1e-3))
+    #spam_predictor = nb.partial_fit(clean_data_np_X,clean_data_np_y,classes=numpy.unique(clean_data_np_y))
+    clf=SGDClassifier(max_iter=1000, tol=1e-3)
+    sgd=clf.partial_fit(clean_data_np_X, clean_data_np_y,classes=numpy.unique(clean_data_np_y))
+    #print(count_batch_number)
+    if count_batch_number==30:
+        filename='sgd_model'
+        with open(filename,'wb') as f:
+            pickle.dump(sgd,f)
+            print('dumped!')
+
+lines.foreachRDD( lambda rdd: readStream(rdd) )
+results = []
+ssc.start()
+ssc.awaitTermination()
