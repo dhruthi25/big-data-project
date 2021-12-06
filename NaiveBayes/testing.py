b3b9d6399339e9f8d6b789f1a22809992ba2e48c
sahanashettigar <r.sahana.shettigar@gmail.com>
2021-12-06 11:00:34 +0530

    NB code done

--- NaiveBayes/testing.py
+++ NaiveBayes/testing.py
@@ -1,5 +1,4 @@
 import numpy
-from pyspark.mllib.evaluation import MulticlassMetrics
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
 from pyspark import SparkContext
@@ -8,15 +7,14 @@ from pyspark.streaming import StreamingContext
 import json
 import pickle
 from pyspark.ml import Pipeline
-from pyspark.ml.feature import Tokenizer,StopWordsRemover, CountVectorizer,IDF,StringIndexer
+from pyspark.ml.feature import RegexTokenizer,StopWordsRemover, CountVectorizer,IDF,StringIndexer
 from pyspark.ml.feature import VectorAssembler
 from pyspark.sql.functions import length
 from pyspark.sql import SparkSession
 from pyspark.sql import Row
-from pyspark.sql.functions import col, split
 from pyspark.ml.feature import CountVectorizer,StopWordsRemover,StringIndexer
 from pyspark.ml.classification import NaiveBayes
-
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, classification_report, confusion_matrix
 sc = SparkContext("local[2]", "sentiment").getOrCreate() #no of threads to run it, cluster name 
 ssc = StreamingContext(sc, 1)
 spark = SparkSession(sc)
@@ -37,33 +35,47 @@ def readStream(rdd):
     df=df['content','verdict']
     #df.show()
     data = df.withColumn('length',length(df['content']))
-    tokenizer = Tokenizer(inputCol="content", outputCol="token_content")
-    stopremove = StopWordsRemover(inputCol='token_content',outputCol='stop_tokens')
-    count_vec = CountVectorizer(inputCol='stop_tokens',outputCol='count_vec')
-    idf = IDF(inputCol="count_vec", outputCol="tf_idf")
-    ham_spam_to_num = StringIndexer(inputCol='verdict',outputCol='label')
-    clean_up = VectorAssembler(inputCols=['tf_idf','length'],outputCol='features')
-    data_prep_pipe = Pipeline(stages=[ham_spam_to_num,tokenizer,stopremove,count_vec,idf,clean_up])
-    cleaner = data_prep_pipe.fit(data)
-    clean_data = cleaner.transform(data)
+    # tokenizer = Tokenizer(inputCol="content", outputCol="token_content")
+    # stopremove = StopWordsRemover(inputCol='token_content',outputCol='stop_tokens')
+    # count_vec = CountVectorizer(inputCol='stop_tokens',outputCol='count_vec')
+    # idf = IDF(inputCol="count_vec", outputCol="tf_idf")
+    # ham_spam_to_num = StringIndexer(inputCol='verdict',outputCol='label')
+    # clean_up = VectorAssembler(inputCols=['tf_idf','length'],outputCol='features')
+    # data_prep_pipe = Pipeline(stages=[ham_spam_to_num,tokenizer,stopremove,count_vec,idf,clean_up])
+    # cleaner = data_prep_pipe.fit(data)
+    # clean_data = cleaner.transform(data)
+    stages=[]
+    regexTokenizer=RegexTokenizer(inputCol='content',outputCol='token',pattern='\\W+')
+    stages+=[regexTokenizer]
+    stopremove = StopWordsRemover(inputCol='token',outputCol='stop_tokens')
+    stages+=[stopremove]
+    cv=CountVectorizer(inputCol='stop_tokens',outputCol='token_features',minDF=2.0,vocabSize=2000)
+    stages+=[cv]
+    indexer=StringIndexer(inputCol='verdict',outputCol='numericlabel')
+    stages+=[indexer]
+    vectorassemble=VectorAssembler(inputCols=['token_features','length'],outputCol="features")
+    stages+=[vectorassemble]
+    pipeline=Pipeline(stages=stages)
+    clean_data=pipeline.fit(data).transform(data)
     clean_data_np_X=numpy.array(clean_data.select('features').collect())
     #print(clean_data_np_X)
     clean_data_np_X=[i.flatten() for i in clean_data_np_X]
     clean_data_np_X=numpy.array(clean_data_np_X)
     #clean_data_np_X=numpy.pad(clean_data_np_X,2741-len(clean_data_np_X),'edge')
-    print(type(clean_data_np_X))
+    #print(type(clean_data_np_X))
     filename='nb_model'
     load_nbmodel=pickle.load(open(filename,'rb'))
     #test_results = load_nbmodel.predict(clean_data_np_X)
-    clean_data_np_y=numpy.array(clean_data.select('label').collect()).flatten()
-    accuracy=load_nbmodel.score(clean_data_np_X,clean_data_np_y)
-    print("Accuracy:{}".format(accuracy))
+    actual_test_op=numpy.array(clean_data.select('numericlabel').collect()).flatten()
+    #accuracy=load_nbmodel.score(clean_data_np_X,actual_test_op)
+    test_output=load_nbmodel.predict(clean_data_np_X)
+
     #metrics = MulticlassMetrics(preds_and_labels.rdd.map(tuple))
     #print(metrics.confusionMatrix().toArray())
-    #print("Accuracy:{}".format(metrics.accuracy))
-    #print("Precision:{}".format(metrics.precision(1.0)))
-    #print("Recall:{}".format(metrics.recall(1.0)))
-    #print("F1 Score:{}".format(metrics.fMeasure(1.0)))
+    #print(accuracy_score(actual_test_op,test_output)*100)
+    #print(precision_score(actual_test_op,test_output))
+    print("Recall:{}".format(recall_score(actual_test_op,test_output)))
+    #print("F1 Score:{}".format(f1_score(actual_test_op,test_output)))
 
 lines.foreachRDD( lambda rdd: readStream(rdd) )
 ssc.start()
--- NaiveBayes/training.py
+++ NaiveBayes/training.py
@@ -7,7 +7,7 @@ from pyspark.streaming import StreamingContext
 import json
 import pickle
 from pyspark.ml import Pipeline
-from pyspark.ml.feature import Tokenizer,StopWordsRemover, CountVectorizer,IDF,StringIndexer
+from pyspark.ml.feature import RegexTokenizer, Tokenizer,StopWordsRemover, CountVectorizer,IDF,StringIndexer
 from pyspark.ml.feature import VectorAssembler
 from pyspark.sql.functions import count, length
 from pyspark.sql import SparkSession
@@ -38,20 +38,34 @@ def readStream(rdd):
     df=spark.createDataFrame((Row(**d) for d in array_of_vals),schema)
     df=df['content','verdict']
     #df.show()
+    #data = df.withColumn('length',length(df['content']))
+    #tokenizer = Tokenizer(inputCol="content", outputCol="token_content")
+    #stopremove = StopWordsRemover(inputCol='token_content',outputCol='stop_tokens')
+    #count_vec = CountVectorizer(inputCol='stop_tokens',outputCol='count_vec')
+    #idf = IDF(inputCol="count_vec", outputCol="tf_idf")
+    #ham_spam_to_num = StringIndexer(inputCol='verdict',outputCol='label')
+    #clean_up = VectorAssembler(inputCols=['tf_idf','length'],outputCol='features')
+    #data_prep_pipe = Pipeline(stages=[ham_spam_to_num,tokenizer,stopremove,count_vec,idf,clean_up])
+    #cleaner = data_prep_pipe.fit(data)
+    #clean_data = cleaner.transform(data)
     data = df.withColumn('length',length(df['content']))
-    tokenizer = Tokenizer(inputCol="content", outputCol="token_content")
-    stopremove = StopWordsRemover(inputCol='token_content',outputCol='stop_tokens')
-    count_vec = CountVectorizer(inputCol='stop_tokens',outputCol='count_vec')
-    idf = IDF(inputCol="count_vec", outputCol="tf_idf")
-    ham_spam_to_num = StringIndexer(inputCol='verdict',outputCol='label')
-    clean_up = VectorAssembler(inputCols=['tf_idf','length'],outputCol='features')
-    data_prep_pipe = Pipeline(stages=[ham_spam_to_num,tokenizer,stopremove,count_vec,idf,clean_up])
-    cleaner = data_prep_pipe.fit(data)
-    clean_data = cleaner.transform(data)
+    stages=[]
+    regexTokenizer=RegexTokenizer(inputCol='content',outputCol='token',pattern='\\W+')
+    stages+=[regexTokenizer]
+    stopremove = StopWordsRemover(inputCol='token',outputCol='stop_tokens')
+    stages+=[stopremove]
+    cv=CountVectorizer(inputCol='stop_tokens',outputCol='token_features',minDF=2.0,vocabSize=2000)
+    stages+=[cv]
+    indexer=StringIndexer(inputCol='verdict',outputCol='numericlabel')
+    stages+=[indexer]
+    vectorassemble=VectorAssembler(inputCols=['token_features','length'],outputCol="features")
+    stages+=[vectorassemble]
+    pipeline=Pipeline(stages=stages)
+    clean_data=pipeline.fit(data).transform(data)
     #clean_data.show()
     clean_data_np_X=numpy.array(clean_data.select('features').collect())
     #print(clean_data_np_X)
-    clean_data_np_y=numpy.array(clean_data.select('label').collect()).flatten()
+    clean_data_np_y=numpy.array(clean_data.select('numericlabel').collect()).flatten()
     clean_data_np_X=[i.flatten() for i in clean_data_np_X]
     clean_data_np_X=numpy.array(clean_data_np_X)
     nb=MultinomialNB()
@@ -59,10 +73,11 @@ def readStream(rdd):
     #print(clean_data_np_y)
     spam_predictor = nb.partial_fit(clean_data_np_X,clean_data_np_y,classes=numpy.unique(clean_data_np_y))
     print(count_batch_number)
-    if count_batch_number==303:
+    if count_batch_number==60:
         filename='nb_model'
         with open(filename,'wb') as f:
             pickle.dump(spam_predictor,f)
+            print('dumped!')
 
 lines.foreachRDD( lambda rdd: readStream(rdd) )
 results = []
